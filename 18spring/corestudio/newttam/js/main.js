de7828bb51e0f4c68d0c40aa6336bc042178b265
leey515 <31672163+leey515@users.noreply.github.com>
2018-05-08 15:56:48 -0400

    aa

---
+++ 18spring/corestudio/newttam/js/main.js
@@ -0,0 +1,292 @@
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
+// This code is adapted from
+// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
+
+'use strict';
+
+/* globals MediaRecorder */
+
+var mediaSource = new MediaSource();
+mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
+var mediaRecorder;
+var recordedBlobs;
+var sourceBuffer;
+
+var gumVideo = document.querySelector('video#gum');
+var recordedVideo = document.querySelector('video#recorded');
+
+var ongoing = false;
+
+// window.isSecureContext could be used for Chrome
+var isSecureOrigin = location.protocol === 'https:' ||
+    location.hostname === 'localhost';
+
+var constraints = {
+    audio: true
+};
+
+function handleSuccess(stream) {
+    console.log('getUserMedia() got stream: ', stream);
+    window.stream = stream;
+    gumVideo.srcObject = stream;
+}
+
+function handleError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+then(handleSuccess).catch(handleError);
+
+function handleSourceOpen(event) {
+    console.log('MediaSource opened');
+    sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+    console.log('Source buffer: ', sourceBuffer);
+}
+
+recordedVideo.addEventListener('error', function(ev) {
+    console.error('MediaRecording.recordedMedia.error()');
+    alert('Your browser can not play\n\n' + recordedVideo.src
+          + '\n\n media clip. event: ' + JSON.stringify(ev));
+}, true);
+
+function handleDataAvailable(event) {
+    if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+function handleStop(event) {
+    console.log('Recorder stopped: ', event);
+}
+
+function toggleRecording() {
+    if (ongoing == false) {
+        startRecording();
+        ongoing = true;
+        console.log("Hi");
+    } else {
+        stopRecording();
+        ongoing = false;
+        console.log("bye");
+    }
+}
+
+function startRecording() {
+    recordedBlobs = [];
+    var options = {mimeType: 'audio/wav;codecs=vp9'};
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.log(options.mimeType + ' is not Supported');
+        options = {mimeType: 'audio/wav;codecs=vp8'};
+        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+            console.log(options.mimeType + ' is not Supported');
+            options = {mimeType: 'audio/wav'};
+            if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+                console.log(options.mimeType + ' is not Supported');
+                options = {mimeType: ''};
+            }
+        }
+    }
+    try {
+        mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (e) {
+        console.error('Exception while creating MediaRecorder: ' + e);
+        alert('Exception while creating MediaRecorder: '
+              + e + '. mimeType: ' + options.mimeType);
+        return;
+    }
+    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+    ongoing = false;
+    mediaRecorder.onstop = handleStop;
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10); // collect 10ms of data
+    console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+    console.log('Recorded Blobs: ', recordedBlobs);
+    recordedVideo.controls = true;
+}
+
+function play() {
+    var superBuffer = new Blob(recordedBlobs, {type: 'audio/wav'});
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+    // workaround for non-seekable video taken from
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=642012#c23
+    recordedVideo.addEventListener('loadedmetadata', function() {
+        if (recordedVideo.duration === Infinity) {
+            recordedVideo.currentTime = 1e101;
+            recordedVideo.ontimeupdate = function() {
+                recordedVideo.currentTime = 0;
+                recordedVideo.ontimeupdate = function() {
+                    delete recordedVideo.ontimeupdate;
+                    recordedVideo.play();
+                };
+            };
+        }
+    });
+}
+
+function download() {
+    var blob = new Blob(recordedBlobs, {type: ''});
+    var url = window.URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'test.webm';
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 100);
+}
+
+var check= false;
+
+function moveUp() {
+    var elem = document.getElementById("up");
+    var pos = 60;
+    var size = 55;
+    var wen = 23;
+    var minspeed = Math.ceil(7);
+    var maxspeed = Math.floor(25);
+    var randomspeed = Math.floor(Math.random()*(maxspeed-minspeed+1))+minspeed;
+    var id = setInterval(frame, randomspeed);
+    function frame() {
+        if (pos == -20) {
+            clearInterval(id);
+        } else {
+            pos--; 
+            size++;
+            wen-= 0.5;
+            elem.style.width = size + '%';
+            elem.style.top = pos + '%';
+            elem.style.left = wen + '%';
+        }
+    }
+}
+
+function sumUp(){
+
+    createimagecenter();
+
+    if ( check == false)
+    {
+        var hello = setInterval(function(){
+            createimage();
+            moveUp();
+
+        },1000);
+
+    }
+    else 
+        {
+            hello = clearInterval(hello);
+        }
+}
+    function toggleHello(){
+        if(check == true)
+            check=false;
+        else
+            check = true;
+    }
+
+
+function createimage(){
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "circle.png");
+    x.setAttribute("id", "up");
+    x.setAttribute("onclick", "moveup()");
+    x.style.top = "60%";
+    x.style.position = "fixed";
+    x.style.width = "55%";
+    x.style.left = "23%";
+    document.body.appendChild(x);
+}
+
+function createimagecenter(){
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "circle.png");
+    x.setAttribute("id", "ups");
+    x.setAttribute("onclick", "moveup()");
+    x.style.top = "63%";
+    x.style.position = "fixed";
+    x.style.width = "55%";
+    x.style.left = "23%";
+    document.body.appendChild(x);
+}
+
+jQuery.fn.shake = function () {
+    this.each(function (i) {
+        $(this).css({
+            "position": "relative"
+        });
+        for (var x = 1; x <= 99999; x++) {
+            $(this).animate({
+                top: 40,
+                left:-6.5
+            }, 15).animate({
+                top: 55,
+                left:-6.5
+            }, 35).animate({
+                top: 55,
+                left:-6.5
+            }, 15).animate({
+                top: 40,
+                left:-6.5
+            }, 35);
+        }
+    });
+
+}
+
+function start() {
+    $('#imgback').shake();
+
+};
+
+var larged = false;
+function sizeUp() {
+    var elem = document.getElementById("down");
+    var pos = 87;
+    var size = 55;
+    var leftPos = 25;
+    if(larged == false){
+        var id = setInterval(frame, 5);
+        function frame() {
+            size++;
+            leftPos-= 0.5;
+            //elem.style.top = pos + '%';
+            elem.style.width = size + '%';
+            elem.style.left = leftPos + '%';
+            if (size == 58) {
+                clearInterval(id);
+            }
+            larged = true;
+        }
+    }
+    else
+    {
+        var id = setInterval(frame, 5);
+        function frame() {
+            size--;
+            leftPos+= 0.5;
+            //elem.style.top = pos + '%';
+            elem.style.width = size + '%';
+            elem.style.left = leftPos + '%';
+            if (size == 50) {
+                clearInterval(id);
+            }
+            larged = false;
+        }
+    }
+}
